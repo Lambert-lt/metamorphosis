4254e8c18c05e24c27e6baacf711723e447e6d5f
Lambert <582001651@qq.com>
2017-03-09 21:53:58 +0800

    本地

---
+++ src/Method.java
@@ -0,0 +1,16 @@
+
+public class Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="1";
+		say(a);
+		System.out.println(a);
+	}
+	
+	private static void say(String a) {
+		// TODO Auto-generated method stub
+		a="2";
+	}
+
+}
