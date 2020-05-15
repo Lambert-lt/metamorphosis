1ca4c28c0183a2f50b9ed8780d42465736c154c1
liutao <582001651@qq.com>
2020-05-15 09:34:45 +0800

    String CheckPermutation

--- src/m202005/StringMethod.java
+++ src/m202005/StringMethod.java
@@ -1,17 +1,19 @@
 package m202005;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class StringMethod {
 
-    public static void main(String[] args) {
+   /*public static void main(String[] args) {
         String astr = "abcdefg";
         String astr1 = "abcddefg";
         System.out.println("astr isUnique: " + isUnique(astr));
         System.out.println("astr1 isUnique: " + isUnique(astr1));
 
-    }
+    }*/
 
     public static boolean isUnique(String astr) {
         Set<Character> strSet = new HashSet();
@@ -33,6 +35,52 @@ public class StringMethod {
             p |= (1 << (astr.charAt(i) - 'a'));
         }
         return true;
+    }
+
+
+    public static boolean CheckPermutation(String s1, String s2) {
+        char[] chars = s1.toCharArray();
+        char[] chars2 = s2.toCharArray();
+
+        Arrays.sort(chars);
+        Arrays.sort(chars2);
+
+        return Arrays.toString(chars).equals(Arrays.toString(chars2));
+    }
+
+ /*   public static void main(String[] args) {
+        String astr = "abcdefg";
+        String astr1 = "abcddefg";
+        System.out.println("astr CheckPermutation: " + CheckPermutation(astr,astr1));
+        String astr2 = "abcdefg";
+        String astr3 = "fgdebca";
+        System.out.println("astr2 CheckPermutation: " + CheckPermutation(astr2,astr3));
+    }*/
+
+
+    public String replaceSpaces(String S, int length) {
+        // 1.查询需要增加的长度
+        int addLength = 0;
+        for (int i = 0; i < length; i++) {
+            if (S.charAt(i) == ' ') {
+                addLength++;
+            }
+        }
+
+        // 2.替换字符串
+
+
+
+        return S;
+    }
+
+    public static void main(String[] args) {
+        String astr = "abcdefg";
+        String astr1 = "abcddefg";
+        System.out.println("astr CheckPermutation: " + CheckPermutation(astr,astr1));
+        String astr2 = "abcdefg";
+        String astr3 = "fgdebca";
+        System.out.println("astr2 CheckPermutation: " + CheckPermutation(astr2,astr3));
     }
 }
 
